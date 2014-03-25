59deeba15a19d7476d2663fc208a41065ad80989
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-25 11:11:05 -0700

    Minor tidy of the configure.zcml

--- gs/group/member/info/memberinfo.py
+++ gs/group/member/info/memberinfo.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
 from zope.cachedescriptors.property import Lazy
-from gs.group.privacy.visibility import GroupVisibility
+from gs.group.privacy import GroupVisibility
 from gs.group.member.viewlet import MemberViewlet
 
 
@@ -24,19 +38,28 @@ class LoggedInMemberInfo(MemberInfo):
 
 
 class PublicGroupMemberInfo(MemberInfo):
-    '''Membership information for public groups.'''
+    '''Membership information (joining) for public groups.'''
     @Lazy
     def show(self):
         retval = not(self.isMember) and self.groupVisibility.isPublic
         return retval
 
 
+class RestrictedGroupMemberInfo(MemberInfo):
+    '''Membership information (joining) for restricted groups.'''
+    @Lazy
+    def show(self):
+        retval = not(self.isMember) and self.groupVisibility.isPublicToSite
+        return retval
+
+
 class PrivateGroupMemberInfo(MemberInfo):
-    '''Membership information for private groups'''
+    '''Membership information (joining) for private groups'''
     @Lazy
     def show(self):
         retval = not(self.isMember) and self.groupVisibility.isPrivate
         return retval
 
 
-# Secret groups are not shown to people we know will be Anonymous.
+# Secret groups are only shown to group members, so there is no joining
+# information.
